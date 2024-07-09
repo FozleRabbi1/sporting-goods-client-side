c38176a322378ef1ca0e62130454b0c0af03adf1
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-07-10 01:02:47 +0600

    desing product cards

--- src/pages/Home/Featuredsection/Cart.tsx
+++ src/pages/Home/Featuredsection/Cart.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Rating from "react-rating";
 import "./Card.css";
+import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
 
-const Cart = (data) => {
+const Cart = (data: any) => {
   const {
     name,
     category,
@@ -11,15 +15,42 @@ const Cart = (data) => {
     price,
     image,
   } = data.item;
-  console.log(data);
   return (
-    <div>
-      <div className="card " key={data._id}>
-        <img className="h-[300px] w-[100%] mx-auto" src={image} alt="" />
-        <div className="card-content">
-          <div className="card-title">{name}</div>
-          <div className="card-description">{description}</div>
-          <div className="card-price">{price}$</div>
+    <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1000">
+      <div className="card border overflow-hidden group" key={data._id}>
+        <img
+          className="h-[300px] w-[100%] mx-auto rounded-t-md transform transition-transform duration-500 group-hover:scale-110"
+          src={image}
+          alt=""
+        />
+        <div className="card-content p-2">
+          <h2 className="card-title">{name}</h2>
+          <p className="card-description text-[14px] opacity-90">
+            {description}
+          </p>
+          <Rating
+            placeholderRating={rating}
+            emptySymbol={<CiStar />}
+            placeholderSymbol={<FaStar className="text-yellow-500" />}
+            fullSymbol={<FaStar />}
+          />
+          <div className="flex justify-between">
+            <span>
+              <p className="text-[12px]">brand : {brand}</p>
+              <h2 className="text-[12px]">category : {category}</h2>
+            </span>
+            <span>
+              <p className="text-[12px]">stock: {stockQuantity}</p>
+              <h2 className="text-[14px] font-semibold text-red-500">
+                price : {price}$
+              </h2>
+            </span>
+          </div>
+        </div>
+        <div className="bg-slate-400 py-1 ">
+          <button className="w-full text-center text-xl font-semibold">
+            View Details
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Home/Featuredsection/Featuredsection.tsx
+++ src/pages/Home/Featuredsection/Featuredsection.tsx
@@ -9,10 +9,20 @@ const Featuredsection = () => {
   }
 
   return (
-    <div className="bg-green-200 grid grid-cols-3 gap-10 px-36 ">
-      {data.data.map((item) => (
-        <Cart key={item._id} item={item}></Cart>
-      ))}
+    <div className="bg-[#edf5f5] ">
+      <h2 className="text-center text-4xl font-bold pt-5">All Products</h2>
+      <p className="px-20 opacity-80 text-center">
+        Tincidunt scelerisque! Ridiculus arcu pulvinar ultrices nascetur
+        scelerisque, rhoncus elementum, elementum, elementum. Pellentesque, nec.
+        Lacus nisi porta ultrices! Ut rhoncus nec nec? Porta dolor, massa
+        elementum nec magnis cum, facilisis montes lacus amet, amet dis risus.
+        Lectus ut, elementum a.
+      </p>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-y-12 md:gap-x-20 p-5 md:p-10 lg:p-20 ">
+        {data?.data?.map((item) => (
+          <Cart key={item._id} item={item}></Cart>
+        ))}
+      </div>
     </div>
   );
 };
