c490fa74e94d431105b89a8c07131312b84a9c42
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-07-14 21:56:41 +0600

    manage Product pase design

--- src/pages/ManageProducts/ManageSIngleCart.tsx
+++ src/pages/ManageProducts/ManageSIngleCart.tsx
@@ -1,41 +1,52 @@
-import React from "react";
-import { allProducts } from "../../redux/fetures/getAllProducts";
-import ManageSIngleCart from "./ManageSIngleCart";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 
-const ManageProducts = () => {
-  const { data, isLoading, refetch } =
-    allProducts.useGetAllProductsQuery(undefined);
-
-  const getData = data?.data;
-
+const ManageSIngleCart = ({ data }) => {
   return (
-    <div className="lg:w-[70%] mx-auto pb-10">
-      <h2 className="md:text-4xl font-bold text-center md:mt-10 md:my-10">
-        Manage ALl Products
-      </h2>
-      <div className="flex justify-end">
-        {" "}
-        <button
-          onClick={() => document.getElementById("my_modal_3").showModal()}
-          className="border w-[100px] md:w-[150px] md:py-2 mb-2 rounded-lg font-semibold"
-        >
-          Add
-        </button>{" "}
-      </div>
-      {isLoading ? (
-        <div className="w-full h-[70vh] flex justify-center items-center">
-          <span className="loading loading-spinner loading-lg"></span>
+    <div className="bg-green-100 mb-4  rounded px-2 py-1">
+      <div className="flex items-center justify-around gap-4 ">
+        <div className="avatar">
+          <div className="mask mask-hexagon size-20 ">
+            <img src={data.image} />
+          </div>
         </div>
-      ) : (
-        getData?.map((item) => (
-          <ManageSIngleCart data={item} key={item._id}></ManageSIngleCart>
-        ))
-      )}
 
-      {/* ==========  dialog ============ */}
-      <dialog id="my_modal_3" className="modal">
+        <span className="flex-1">
+          <h2 className="text-[12px] sm:text-[16px] font-semibold">
+            {" "}
+            {data.name}
+          </h2>
+          <h2 className="text-[12px] sm:text-[16px]">Brand : {data.brand}</h2>
+        </span>
+
+        <span className="flex-1">
+          <h2 className="text-[12px] sm:text-[16px] font-semibold">
+            {" "}
+            {data.category}
+          </h2>
+          <h2 className="text-[12px] sm:text-[16px]">Price : {data.price}</h2>
+        </span>
+
+        <span className="flex-1">
+          <h2 className="text-[12px] sm:text-[16px] font-semibold">
+            {" "}
+            Stock : {data.stockQuantity}
+          </h2>
+          <h2 className="text-[12px] sm:text-[16px]">Rating : {data.rating}</h2>
+        </span>
+
+        <span className="flex flex-col md:flex-row items-center justify-center">
+          <FaRegEdit
+            onClick={() => document.getElementById("my_modal_4").showModal()}
+            className="cursor-pointer text-green-500 text-[8px] border size-7 md:size-8 rounded-lg hover:text-green-700 duration-500 sm:text-[26px] font-semibold p-0.5 "
+          />
+          <MdDeleteForever className="cursor-pointer text-red-500 text-[8px] border size-7 md:size-8 rounded-lg hover:text-red-700 duration-500 sm:text-[26px] font-semibold p-0.5 md:ml-4" />
+        </span>
+      </div>
+
+      <dialog id="my_modal_4" className="modal">
         <div className="modal-box">
-          <h2 className="text-center font-semibold">Added Your Product</h2>
+          <h2 className="text-center font-semibold">Update Your Product</h2>
           <form method="dialog">
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
@@ -103,4 +114,4 @@ const ManageProducts = () => {
   );
 };
 
-export default ManageProducts;
+export default ManageSIngleCart;
